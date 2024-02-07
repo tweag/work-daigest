f1bc72d848961ef27819568ce7a543125c8d4c68
Simeon Carstens <simeon.carstens@tweag.io>
2024-02-07 14:28:34 +0100

    Fetch GitHub issue comments

--- fetchers/github.py
+++ fetchers/github.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 import datetime
+import dateutil.parser
+import requests
 from typing import NewType
 
 CommentText = NewType("CommentText", str)
@@ -11,11 +13,33 @@ class GitHubComment:
     date: datetime.datetime
     text: CommentText
     repository: RepositoryName
-    comment_type: CommentType
 
+
+BASE_URL = "https://api.github.com/search"
+
+def fetch_issue_comments(handle: str) -> list[GitHubComment]:
+    response = requests.get(f"{BASE_URL}/issues?q=commenter:{handle}")
+    return [
+        GitHubComment(
+            # use datetime string of form "YYYY-MM-DDTHH:MM:SSZ" and convert into datetime object
+            # TODO: could also try to use "updated_at" or "closed_at" fields
+            dateutil.parser.parse(comment_json["created_at"]),
+            CommentText(comment_json["body"]),
+            # example repo URL: https://api.github.com/repos/tweag/chainsail
+            # so we use "tweag/chainsail" as human-readable repo identifier
+            RepositoryName("/".join(comment_json["repository_url"].split("/")[-2:]))
+        )
+
+        for comment_json in response.json()["items"]
+    ]
 
 def fetch_comments(handle: str) -> list[GitHubComment]:
     """
     Fetch all GitHub comments authored by user `handle`
     """
-    ...
+    all_comments = []
+    all_comments.append(fetch_issue_comments(handle))
+    return all_comments
+
+if __name__ == "__main__":
+    print(fetch_comments("simeoncarstens")[:10])
