a4074cbc8a6f1e694884f134e250a7bfd613f99f
Simeon Carstens <simeon.carstens@tweag.io>
2024-03-21 14:13:06 +0100

    Clean up prompt template definition

--- week_dAIgest/main.py
+++ week_dAIgest/main.py
@@ -10,6 +10,25 @@ from ics import Calendar
 
 model_name = "meta.llama2-70b-chat-v1"
 #model_name = "ai21.j2-jumbo-instruct"
+PROMPT_TEMPLATE = """
+    My name is Simeon Carstens and I am a software engineer at Tweag.
+    Summarize the events in the calendar and my work on GitHub and tell me what I did this week.
+    If the event has a description, include a summary.
+    Include attendees names.
+    If the event is lunch, do not include it.
+    For GitHub issues / pull requests / commits, don't include the full text / description / commit message,
+    but summarize it if it is longer than two sentences.
+
+    Calendar events:
+    ```
+    {calendar_data}
+    ```
+
+    These are GitHub issues, pull requests and commits I worked on, in a JSON format:
+    ```
+    {github_data}
+    ```
+    """
 
 def munge_calendar_data(file_path: str, min_date: datetime.datetime, max_date: datetime.datetime, email: str) -> str:
     """
@@ -54,27 +73,12 @@ calendar_data = munge_calendar_data("calendar.ics", datetime.datetime.now() - da
 
 runtime_client = init_client('bedrock-runtime', 'us-east-1')
 
-prompt_template = f"""
-    My name is Simeon Carstens and I am a software engineer at Tweag.
-    Summarize the events in the calendar and my work on GitHub and tell me what I did this week.
-    If the event has a description, include a summary.
-    Include attendees names.
-    If the event is lunch, do not include it.
-    For GitHub issues / pull requests / commits, don't include the full text / description / commit message,
-    but summarize it if it is longer than two sentences.
 
-    Calendar events:
-    ```
-    {calendar_data}
-    ```
-
-    These are GitHub issues, pull requests and commits I worked on, in a JSON format:
-    ```
-    {json.dumps(github_data)}
-    ```
-    """
 
 # res = invoke_llama2(runtime_client, model_id=model_name, prompt=prompt_template)
 # res = invoke_jurassic2(runtime_client, jurassic_model=model_name, prompt=prompt_template)
-res = invoke_claude3(runtime_client, prompt=prompt_template)
+res = invoke_claude3(
+    runtime_client,
+    prompt=PROMPT_TEMPLATE.format(calendar_data=calendar_data, github_data=github_data)
+)
 print(res)
