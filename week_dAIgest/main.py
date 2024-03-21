ab058f30760bf6ef6c015fb2167e07c51358332d
Simeon Carstens <simeon.carstens@tweag.io>
2024-03-21 12:34:29 +0100

    Put calendar / GitHub data loading / munging into functions

--- week_dAIgest/main.py
+++ week_dAIgest/main.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import datetime
+import json
 import os
 from pickle import dump, load
 
@@ -6,30 +7,50 @@ from .bedrock import init_client, invoke_claude3, invoke_llama2, invoke_jurassic
 from .fetchers.google_calendar import format_events
 
 from ics import Calendar
-import pytz
 
 model_name = "meta.llama2-70b-chat-v1"
 #model_name = "ai21.j2-jumbo-instruct"
 
-if os.path.exists("caldump.pickle"):
-    with open("caldump.pickle", "rb") as f:
-        cal_text = load(f)
-else:
-    calendar_file = 'calendar.ics'
-    email = "simeon.carstens@tweag.io"
-    d1 = datetime(2024, 1, 29, tzinfo=pytz.timezone("CET"))
-    d2 = datetime(2024, 2, 4, tzinfo=pytz.timezone("CET"))
+def munge_calendar_data(file_path: str, min_date: datetime.datetime, max_date: datetime.datetime, email: str) -> str:
+    """
+    Munge calendar data to be used in the prompt template.
 
-    with open(calendar_file, 'r') as f:
-        print("Reading calendar from file")
-        calendar = Calendar(f.read())
+    :param file_path: Path to the calendar file.
+    :param min_date: Minimum date to consider.
+    :param max_date: Maximum date to consider.
+    :param email: Email to filter calendar events.
+    :return: Munged calendar data.
+    """
+    # Cache munged calendar data
+    CACHE_FILE = "caldump.pickle"
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, "rb") as f:
+            cal_text = load(f)
+    else:
+        with open(file_path, 'r') as f:
+            calendar = Calendar(f.read())
 
-    cal_text = format_events(calendar, d1, d2, email)
-    with open("caldump.pickle", "wb") as f:
-        dump(cal_text, f)
+        cal_text = format_events(calendar, min_date, max_date, email)
+        with open(CACHE_FILE, "wb") as f:
+            dump(cal_text, f)
 
-import json
-github_data = json.load(open("github_data.json"))
+    return cal_text
+
+def munge_github_data(file_path: str) -> str:
+    """
+    Munge GitHub data to be used in the prompt template.
+
+    :param file_path: Path to the JSON file containing GitHub data
+      as produced by the GitHub fetcher.
+    :return: Munged GitHub data.
+    """
+    with open(file_path, 'r') as f:
+        github_data = json.load(f)
+
+    return json.dumps(github_data)
+
+github_data = munge_github_data("github_data.json")
+calendar_data = munge_calendar_data("calendar.ics", datetime.datetime.now() - datetime.timedelta(days=60), datetime.datetime.now(), "simeon.carstens@tweag.io")
 
 runtime_client = init_client('bedrock-runtime', 'us-east-1')
 
@@ -44,7 +65,7 @@ prompt_template = f"""
 
     Calendar events:
     ```
-    {cal_text}
+    {calendar_data}
     ```
 
     These are GitHub issues, pull requests and commits I worked on, in a JSON format:
